6144934c804d59a04030f01f68ec6cf8b0997516
Peter Deppe <1427864+depster26@users.noreply.github.com>
2021-04-17 10:08:59 -0700

    added allow methods and headers to response headers

--- lambda_proxy_helpers_pkg/lambda_proxy_response.py
+++ lambda_proxy_helpers_pkg/lambda_proxy_response.py
@@ -73,6 +73,8 @@ class LambdaProxyResponse:
             "headers": {
                 "Content-Type": 'application/json',
                 "Access-Control-Allow-Origin": '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Headers': '*',
                 "Access-Control-Allow-Credentials": True
             }
         }
